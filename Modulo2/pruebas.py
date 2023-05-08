a20c5e8555c20952e28858028c6d6cc7948807cb
Anderson Gonzalez Acosta <pandabitxx@gmail.com>
2023-05-08 16:26:26 -0500

    Week of May 1 Andes

---
+++ Modulo2/pruebas.py
@@ -0,0 +1,22 @@
+# def suma_de_numeros(division):
+#     A = input('Ingrese el primer numero')
+#     B = input('Ingrese el segundo numero')
+#     C = A/B
+#     print(division)'
+    
+    
+# def division():
+#     num1 = float(input("Ingrese el primer número: "))
+#     num2 = float(input("Ingrese el segundo número: "))
+#     resultado = num1 / num2
+#     print("El resultado de la división es: ", resultado)
+
+# division()
+
+def suma_de_numeros():
+    A = float(input('Ingrese el primer numero: '))
+    B = float(input('Ingrese el segundo numero: '))
+    C = A / B
+    print(C)
+
+suma_de_numeros()
